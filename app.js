a91af9aa1e2ade984622aef7240714cd3b2db690
pyuser <enes.karakas.e@gmail.com>
2021-10-20 17:20:06 +0300

    tours api added

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
-
+const fs = require("fs");
 const app = express();
+
+const toursData = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    result: toursData.length,
+    data: {
+      tours: toursData,
+    },
+  });
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("get works");
